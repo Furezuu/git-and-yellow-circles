2e4e7ab85f2195b37738fca1ad9e66620efa2964
MOVS Laboratory <lab.movs@outlook.com>
2019-11-11 19:07:45 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+
+class YellowCirclesForm(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        pass
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    wnd = YellowCirclesForm()
+    wnd.show()
+    sys.exit(app.exec())
