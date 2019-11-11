c65d84b6f584a4e2a52e1fb34efa22c70d3a1a9b
MOVS Laboratory <lab.movs@outlook.com>
2019-11-11 19:11:42 +0500

    Implement class Circle

--- main.py
+++ main.py
@@ -3,10 +3,18 @@ from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5 import uic
 
 
+class Circle:
+    def __init__(self, x, y, rad):
+        self.x, self.y, self.r = x, y, rad
+
+
 class YellowCirclesForm(QWidget):
     def __init__(self):
         super().__init__()
         uic.load('UI.ui', self)
+
+
+
         self.init_ui()
 
     def init_ui(self):
