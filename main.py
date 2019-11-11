a0787db44c31710a856e6e58e74c5f376e78ed37
MOVS Laboratory <lab.movs@outlook.com>
2019-11-11 19:09:01 +0500

    Add ui file and loaded it to app

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5 import uic
 
 
 class YellowCirclesForm(QWidget):
     def __init__(self):
         super().__init__()
+        uic.load('UI.ui', self)
         self.init_ui()
 
     def init_ui(self):
